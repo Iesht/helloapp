1b88a0d99de241a8bd81e40ce5aa0d579f98e263
white <white@example.com>
2023-11-06 10:10:31 +0500

    White commit

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+internal class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("hello");
+    }
+
+    public static void GreetWhite()
+    {
+        Console.WriteLine("hello white");
     }
 }
